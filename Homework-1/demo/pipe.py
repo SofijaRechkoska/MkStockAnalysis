d8b2e5f6436f4a3a2781331e6fa4759382f33b59
S <116579626+SofijaRechkoska@users.noreply.github.com>
2024-11-10 00:58:09 +0100

    Delete Homework-1/demo directory

--- Homework-1/demo/pipe.py
+++
@@ -1,8 +0,0 @@
-from demo.Filter1 import starting_filter1
-from demo.Filter2 import starting_filter2
-from demo.Filter3 import starting_filter3
-
-if __name__ == "__main__":
-    starting_filter1() #getting codes
-    starting_filter2() #gettingHistoricData
-    starting_filter3() #updatingData
